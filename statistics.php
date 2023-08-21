6390e49bc05e12d3e538b41224e47673b14950b4
Markus Faßbender <markus.fassbender@gmail.com>
2023-08-21 19:02:15 +0200

    set version

--- statistics.php
+++ statistics.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Statistiken V 3.2.1 (kompatibel mit Admidio 4.1)
+ * Statistiken V 3.3.0 (kompatibel mit Admidio 4.1 and above)
  *
  * Beta-Version
  *
@@ -9,7 +9,7 @@
  * geladen und angezeigt werden.
  *
  *
- * @copyright 2004-2021 The Admidio Team
+ * @copyright 2004-2023 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  *
